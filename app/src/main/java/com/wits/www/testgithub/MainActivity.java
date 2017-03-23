7036de1fa81b7cb25b85cc55ecdcc87aa4a17f6e
hlg <826788352@qq.com>
2017-03-23 14:01:39 +0800

    add onstart

--- app/src/main/java/com/wits/www/testgithub/MainActivity.java
+++ app/src/main/java/com/wits/www/testgithub/MainActivity.java
@@ -1,5 +1,7 @@
 package com.wits.www.testgithub;
 
+import android.content.SyncStatusObserver;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.print("add ahha");
     }
 }
